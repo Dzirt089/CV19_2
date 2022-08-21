11f68d70cb070afcac3b105447ea668e7e2bb518
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2022-08-21 09:52:29 +0300

    Тестирование работы модели представления

--- CV19_2/ViewModels/MainWindowViewModel.cs
+++ CV19_2/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CV19_2.ViewModels.Base;
 
 namespace CV19_2.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "Анализ статистики CV19";
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+            //set
+            //{
+            //    //Этот код должен быть в каждом свойстве. 
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+            //    //Так как этот код мы вписали в метод Set, то мы делаем так:
+                
+            //    Set (ref _Title, value);
+            //}
+        }
+        #endregion
     }
 }
