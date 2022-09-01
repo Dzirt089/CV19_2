e82f559f6ad44c187f93dcba8728d3f070177d90
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2022-09-01 16:50:02 +0300

    Базовый класс комманды

---
+++ CV19_2/Infrastructures/Commands/Base/Command.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CV19_2.Infrastructures.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        /// <summary>
+        /// Передаем управление событием "event EventHandler CanExecuteChanged" системе WPF. Для этого реализуем события явно. Таким образом, что
+        /// передаем управление событием классу CommandManager и WPF автоматичиски генерирует это событие у всех команд, когда что-то происходит
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value; 
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        /// <summary>
+        /// CanExecute - это функция, которая возвращает либо истину, либо ложь  
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public abstract bool CanExecute(object parameter);
+        /// <summary>
+        /// Метод Execute, это то - что должно быть выполнено самой коммандой. Это основная логика команды, которая она должна выполнять
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public abstract void Execute(object parameter);
+}
