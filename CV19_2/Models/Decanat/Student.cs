9aa90016f776a33062ec08da0b561709fab01531
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2022-09-04 17:04:28 +0300

    Тестовая предметная область

---
+++ CV19_2/Models/Decanat/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19_2.Models.Decanat
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public double Rating { get; set; }
+
+    }
+
+    internal class Group
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Свойство группы задаем ICollection<>, чтобы получить свободу выбора - какую коллекцию сюда добавлять. Это может быть Список, Массив, и все чо угодно, что может хранить студентов 
+        /// </summary>
+        public ICollection<Student> Students { get; set; }
+
+    }
+
+}
